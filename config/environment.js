f89fe82366922a836edbc22b1c000460f3c93717
Tom Leskin <tleskin@gmail.com>
2016-07-18 16:59:03 -0400

    Fix prefix

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'tleskin.github.io',
+    modulePrefix: '/',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
