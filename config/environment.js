3e84c010c808daf9678ea3f4c05486f653ca225e
Tom Leskin <tleskin@gmail.com>
2016-07-18 16:57:50 -0400

    Change prefix

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'tomleskin',
+    modulePrefix: 'tleskin.github.io',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
